786a9256b43c6774300641e47f6f512090c6224f
abdulkadir <abdcelikkanat@gmail.com>
2019-09-22 08:24:26 +0200

    zorunlu commit

--- link_prediction/read_scores.py
+++ link_prediction/read_scores.py
@@ -6,17 +6,20 @@ import pickle
 
 # facebook_combined_deepwalk
 
-base_folder = "../edge_prediction" # "./scores"
-generic_name = "facebook_combined_gcc" #"p2p-Gnutella08_gcc" #"facebook_combined"
-method_name = "deepwalk"
+base_folder = "../sil_kesin/results" # "./scores"
+generic_name = "p2p-Gnutella04_gcc_residual" #"p2p-Gnutella08_gcc" #"facebook_combined"
+#method_name = "expfam_norm_alpha=005"
+method_name = "expfam_norm"
+
+#method_name = "expfam_norm"
 
 subfolder = "" #generic_name
-file_path = os.path.join(base_folder, subfolder, generic_name+"_"+method_name+".scores" )
-
+#file_path = os.path.join(base_folder, subfolder, generic_name+"_"+method_name+".pkl" )
+file_path = sys.argv[1]
 
 pf = pickle.load(open(file_path, 'rb'))
 
 total_scores = pf #pf[0]
 
 for metric in total_scores:
-	print("{}: {}".format(metric, total_scores[metric]))
+	print("{}: {}".format(metric, total_scores[metric]['test'][0]))
